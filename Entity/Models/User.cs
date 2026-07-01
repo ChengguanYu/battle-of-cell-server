920bae5ca4a2c4ee986d434a24a7280d2ba94a13
weiyc <csway1024@gmail.com>
2026-07-01 16:39:38 +0800

    refactor(user): 添加uuid\email

--- Entity/Models/User.cs
+++ Entity/Models/User.cs
@@ -3,10 +3,17 @@ using SqlSugar;
 namespace Entity.Models;
 
 [SugarTable("users")]
+[SugarIndex("uq_user_uuid", nameof(Uuid), OrderByType.Asc, true)]
 public class User : EntityBase
 {
-    [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+    [SugarColumn(ColumnName = "s_user_id", IsPrimaryKey = true, IsIdentity = true)]
     public long Id { get; set; }
+
+    [SugarColumn(ColumnDataType = "uuid", IsNullable = false)]
+    public Guid Uuid { get; set; }
+    
+    [SugarColumn(Length = 256, IsNullable = false)]
+    public string Email { get; set; } = string.Empty;
 
     [SugarColumn(Length = 64, IsNullable = false)]
     public string Username { get; set; } = string.Empty;
